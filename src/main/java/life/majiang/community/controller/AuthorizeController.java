ae65815a18cc5c7e61a3b2766733e07f1c163b6c
movi <122365119@qq.com>
2020-01-07 15:00:22 +0800

    add flyway migration support

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import sun.net.httpserver.HttpServerImpl;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +36,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state,
-                           HttpServletRequest request ){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTockenDTO accessTockenDTO = new AccessTockenDTO();
         accessTockenDTO.setClient_id(clientID);
         accessTockenDTO.setClient_secret(clientSecret);
@@ -44,15 +47,16 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTockenDTO);
         GithubUser githubUser = githubProvider.getuser(accessToken);
         if (githubUser != null) {
-            //登陆成功，写cookie和session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            //登陆成功，写cookie和session
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else{
             //登陆失败，重新登陆
